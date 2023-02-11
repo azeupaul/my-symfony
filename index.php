feedb1fb2bf494a1b059de3e31361d1ee5429b64
azeupaul <azeupaul@gmail.com>
2023-02-12 00:50:53 +0100

    Remove multiple include

--- index.php
+++ index.php
@@ -6,11 +6,12 @@ require __DIR__ . '/vendor/autoload.php';
 
 $request = Request::createFromGlobals();
 
+$map = [
+    '/' => 'home.php',
+    '/bye' => 'bye.php',
+];
+
 // Get the current URI
 $pathInfo = $request->getPathInfo();
 
-if ($pathInfo === '/') {
-    include(__DIR__ . '/src/pages/home.php');
-} else {
-    include(__DIR__ . '/src/pages/bye.php');
-}
+include __DIR__ . '/src/pages/' . $map[$pathInfo];
--- src/pages/home.php
+++ src/pages/home.php
@@ -5,7 +5,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 $request = Request::createFromGlobals();
 
-$name = $request->query->get('name', 'Pablo');
+$name = $request->query->get('name', 'World');
 
 $response = new Response();
 $response->headers->set('Content-Type', 'text/html; charset=utf-8');
