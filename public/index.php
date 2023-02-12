4188c24a8f2903785c3cbf9f333ab6327c0f2bf7
azeupaul <azeupaul@gmail.com>
2023-02-12 01:55:03 +0100

    Add symfony/routing and use it for our routing

--- public/index.php
+++ public/index.php
@@ -2,28 +2,38 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $request = Request::createFromGlobals();
 
-$response = new Response();
+$response = new Response;
 
-$map = [
-    '/' => 'home.php',
-    '/bye' => 'bye.php',
-    '/about' => 'about.php',
-];
+$routes = new RouteCollection;
+$routes->add('hello', new Route('/hello/{name}', ['name' => 'World']));
+$routes->add('bye', new Route('/bye'));
+$routes->add('about', new Route('/about'));
+
+$context = new RequestContext;
+$context->fromRequest($request);
+
+$urlMatcher = new UrlMatcher($routes, $context);
 
 // Get the current URI
-$pathInfo = $request->getPathInfo();
+$path = $request->getPathInfo();
 
-if (isset($map[$pathInfo])) {
+try {
+    $result = $urlMatcher->match($path);
     extract($request->query->all());
     ob_start();
-    include __DIR__ . '/../src/pages/' . $map[$pathInfo];
+    include __DIR__ . '/../src/pages/' . $result['_route'] . '.php';
     $response->setContent(ob_get_clean());
-} else {
+} catch (ResourceNotFoundException $e) {
     $response->setContent("Page not found");
     $response->setStatusCode(404);
 }
