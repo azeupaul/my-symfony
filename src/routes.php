2104f33063ef9a5c30857b102ab97ce46977f325
azeupaul <azeupaul@gmail.com>
2023-02-25 14:09:53 +0100

    Change closure to class into route callback

--- src/routes.php
+++ src/routes.php
@@ -3,24 +3,39 @@
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
+class Hello
+{
+    public function index($request)
+    {
+        return render_template($request);
+    }
+
+    public function bye($request)
+    {
+        return render_template($request);
+    }
+}
+
+class About
+{
+    public function index($request)
+    {
+        return render_template($request);
+    }
+}
+
 $routes = new RouteCollection;
 $routes->add('hello', new Route('/hello/{name}', [
     'name' => 'World',
-    '_controller' => function ($request) {
-        return render_template($request);
-    }
+    '_controller' => [new Hello(), 'index']
 ]));
 
 $routes->add('bye', new Route('/bye', [
-    '_controller' => function ($request) {
-        return render_template($request);
-    }
+    '_controller' => [new Hello, 'bye']
 ]));
 
 $routes->add('about', new Route('/about', [
-    '_controller' => function ($request) {
-        return render_template($request);
-    }
+    '_controller' => [new About, 'index']
 ]));
 
 return $routes;
