d74c98485d667c12895ed03e477fd0506da49622
azeupaul <azeupaul@gmail.com>
2023-02-25 15:11:06 +0100

    Add space

--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,7 @@ use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
 $routes = new RouteCollection;
+
 $routes->add('hello', new Route('/hello/{name}', [
     'name' => 'World',
     '_controller' => 'App\Controllers\HelloController::index'
