ad09219b999fc4627d80dd70e08b6c96147bc7b3
azeupaul <azeupaul@gmail.com>
2023-02-25 13:53:15 +0100

    Improved performance router and use call_user_func for rendering page

--- public/index.php
+++ public/index.php
@@ -1,10 +1,11 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Matcher\CompiledUrlMatcher;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\Dumper\CompiledUrlMatcherDumper;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -15,16 +16,25 @@ $routes = require(__DIR__ . '/../src/routes.php');
 $context = new RequestContext;
 $context->fromRequest($request);
 
-$urlMatcher = new UrlMatcher($routes, $context);
+$compiledRoutes = (new CompiledUrlMatcherDumper($routes))->getCompiledRoutes();
+
+$matcher = new CompiledUrlMatcher($compiledRoutes, $context);
+
+function render_template($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
 
 try {
-    extract($urlMatcher->match($request->getPathInfo()));
-    ob_start();
-    include __DIR__ . '/../src/pages/' . $_route . '.php';
-    $response = new Response(ob_get_clean());
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func('render_template', $request);
 } catch (ResourceNotFoundException $e) {
     $response = new Response("<h1>Page not found</h1>", 404);
-} catch (Exception $e) {
+} catch (Exception $exception) {
     $response = new Response("<h1>An error occured</h1>", 500);
 }
 
